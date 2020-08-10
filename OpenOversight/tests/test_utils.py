a7933a26506a9e0493a672c63f5e4766a8df12a0
Julia Holgado <jholgado@newrelic.com>
2020-08-09 18:28:56 -0700

    fix utils test cases

--- OpenOversight/tests/test_utils.py
+++ OpenOversight/tests/test_utils.py
@@ -38,11 +38,9 @@ def test_gender_filter_select_all_male_or_not_sure_officers(mockdata):
     )
 
     result_genders = [officer.gender for officer in results.all()]
-    # TODO: insert officer data that has null gender. this test will fail since 
-    # filter by form now ignores Not Sure values
     for element in results.all():
-        assert element.gender in ('M', 'Not Sure')
-    for gender in  ('M', 'Not Sure'):
+        assert element.gender in ('M', None)
+    for gender in  ('M', None):
         assert gender in result_genders
 
 def test_gender_filter_include_all_genders_if_not_sure(mockdata):
@@ -52,7 +50,7 @@ def test_gender_filter_include_all_genders_if_not_sure(mockdata):
     )
    
     result_genders = [officer.gender for officer in results.all()]
-    for gender in  ('M', 'F', 'Not Sure'):
+    for gender in  ('M', 'F', 'Other', None):
         assert gender in result_genders
     for officer in results.all():
         assert officer.department == department
